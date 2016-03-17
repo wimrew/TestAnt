ff9b8b56e967e7dd3ec30cc701b9af0b29481f8b
Vekov Imre <wimrew@gmail.com>
2016-03-17 11:59:57 -0400

    JUnit test

---
+++ test/controller/MainTest.java
@@ -0,0 +1,16 @@
+package controller;
+
+import org.junit.Test;
+
+
+public class MainTest {
+	
+	@Test
+	public void testMain() throws Exception{
+		
+		TestAntMain m = new TestAntMain();
+		
+		
+	}
+
+}

6b95fa5730d64fef95fa3c2292c167e126b8d021
Vekov Imre <wimrew@gmail.com>
2016-03-17 10:21:30 -0400

    transform to JFrame

--- src/controller/TestAnt.java
+++ src/controller/TestAnt.java
@@ -1,10 +1,20 @@
 package controller;
 
-public class TestAnt {
+import java.awt.Container;
+
+import javax.swing.JFrame;
+
+public class TestAnt extends JFrame{
 
 	public static void main(String[] args){
 		System.out.println("Aaaaaaaaant!fffff");
+		new TestAnt();
 	}
 	
-	
+	public TestAnt(){
+		Container container = getContentPane();
+
+		this.setVisible(true);
+
+	}
 }

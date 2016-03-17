0acba538d9b577f9fffcb99a2911aa3c9ab2ec0b
Vekov Imre <wimrew@gmail.com>
2016-03-17 11:50:28 -0400

    Ant build xmls created

---
+++ src/controller/TestAntMain.java
@@ -0,0 +1,20 @@
+package controller;
+
+import java.awt.Container;
+
+import javax.swing.JFrame;
+
+public class TestAntMain extends JFrame{
+
+	public static void main(String[] args){
+		System.out.println("Aaaaaaaaant!fffff");
+		new TestAntMain();
+	}
+	
+	public TestAntMain(){
+		Container container = getContentPane();
+
+		this.setVisible(true);
+
+	}
+}
